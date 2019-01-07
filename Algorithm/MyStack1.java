fb29ab7a2e9b163c31bea87e25c2afde4d1c2e15
Larry Stones <41537013+larrystones88@users.noreply.github.com>
2019-01-07 22:18:19 +0800

    Update MyStack1.java

--- Algorithm/MyStack1.java
+++ Algorithm/MyStack1.java
@@ -10,5 +10,38 @@ package javaapplication8;
  * @author leilei
  */
 public class MyStack1 {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
     
+    public MyStack1(){
+        this.stackData = new Stack<Integer>();
+        this.stackMin = new Stack<Integer>();
+    }
+    
+    public int getmin(){
+        if(this.stackMin.isEmpty()){
+            throw new RuntimeException("empty");
+        }
+        return this.stackMin.peek();
+    }
+    
+    public void push(int newNum){
+        if(this.stackMin.isEmpty()){
+            this.stackMin.push(newNum);
+        }else if(newNum <= this.getmin()){
+            this.stackMin.push(newNum);
+        }
+        this.stackData.push(newNum);
+    }
+    
+    public int pop(){
+        if(this.stackData.isEmpty()){
+            throw new RuntimeException("empty");
+        }
+        int value = this.stackData.pop();
+        if(value == this.getmin()){
+            this.stackMin.pop();
+        }
+        return value;
+    }
 }
